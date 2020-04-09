6c6e08ba4fae46b059e1ab4c5633c0349e117af6
nshalon <nitanshalon@gmail.com>
2020-04-09 10:36:39 -0700

    fix global

--- global.mk
+++ global.mk
@@ -1,14 +1,21 @@
+###################################################
+# TOOL AND SCRIPT  DIRECTORIES, EDIT AS NEEDED
+###################################################
+
 _recycle_dir?=/home/nshalon/python/python3/bin
 _read_dir?=/relman02/data/public/HMP/shotgun_healthy_gut
 _bwa?=/relman02/users/eitany/download/bwa-0.7.12/bwa
-_py?=/home/nshalon/work/pipe/md/Python
-_pl?=/home/nshalon/work/pipe/md/pl
-_r?=/relman03/work/users/nshalon/pipe/md/R
+_py?=md/Python
+_pl?=md/pl
+_r?=md/R
 _mega?=/home/dethlefs/bin
 _nucmer?=/home/nshalon/work/pipe/mummer-4.0.0beta2
 varisum_dir?=/relman02/users/eitany/ms_root/makeshift-modules/vari//bin.koch.stanford.edu/
+
+# DEFAULT KMER SIZE FOR ASSEMBLY
 _k?=77
 
+# DB FOR ANNOTATION
 UNIREF_DIAMOND_DB_DIR=/relman03/work/users/eitany/bcc/diamond_db
 GO_BASE_DIR=/relman03/work/users/eitany/bcc/GO
 
